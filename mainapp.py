f1a36146420eec33c88376ea0d2f94bc5e6225a8
venky <venkyabcdefg7@gmail.com>
2025-11-15 21:39:23 +0530

    Added requirements.txt and Procfile for Render deployment

--- mainapp.py
+++ mainapp.py
@@ -17,11 +17,14 @@ app.secret_key = "dev-secret"
 # ---------------------------------
 # MySQL Configuration
 # ---------------------------------
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'example_user'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'restaurent_db'
-app.config['MYSQL_PORT'] = 3306
+import os
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
+
 
 mysql = MySQL(app)
 
